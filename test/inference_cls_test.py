d7d2a0117d064387cdac5e1e9f69ed1774d1f8c6
Karnsira <karnsira.s@gmail.com>
2022-09-13 19:47:44 +0700

    add test

---
+++ test/inference_cls_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+class TestClassificationInference(unittest.TestCase):
+    def test_inference_success(self):
+        actual = ''
+        expected = ''
+        self.assertEqual(actual, expected)
+
+    def test_inference_fail(self):
+        actual = ''
+        expected = ' '
+        self.assertNotEqual(actual, expected)
+
+if __name__ == '__main__' : 
+    unittest.main()
